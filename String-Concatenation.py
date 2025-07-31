c1f141bd9fd5a699efdabbaa1b0bf34eb5ac0b42
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-07-31 21:14:08 +0100

    Update String-Concatenation.py

--- String-Concatenation.py
+++ String-Concatenation.py
@@ -1,6 +1,6 @@
 str1 = "Hello There"
-str2 = "I have alwasy wondered what Python was!
+str2 = "I have alwasy wondered what Python was!"
 str3 = "Was it a snake, or just some weird nomenclature"
 
-result = str1 + " ! " + str2 " " + str3
+result = str1 + " ! " + str2 + " " + str3
 print(result)
