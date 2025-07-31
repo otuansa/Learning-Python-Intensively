220231503a6179e2224450e2f159765751362f7f
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-07-31 21:12:54 +0100

    Create String-Len.py
    A short overview of the inbuilt python function of Length (len)

---
+++ String-Len.py
@@ -0,0 +1,4 @@
+#This is a way to count the characters or length of a string
+text = "I am learning to take one python study day at a time. That way I am able to keep tab on the length of each topic convered and conquered"
+length = len(text)
+print("Length of the string:", length)
